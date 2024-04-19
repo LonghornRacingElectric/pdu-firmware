e2c8834a4748222f5f50d7f2da917acf54dfc4fd
matt-mekha <matthew.mekha@gmail.com>
2024-04-19 00:35:14 -0500

    fixed ioc

--- Core/Src/lptim.c
+++ Core/Src/lptim.c
@@ -119,7 +119,7 @@ void HAL_LPTIM_MspInit(LPTIM_HandleTypeDef* lptimHandle)
     */
     GPIO_InitStruct.Pin = Tach_RadFan_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF1_LPTIM1;
     HAL_GPIO_Init(Tach_RadFan_GPIO_Port, &GPIO_InitStruct);
@@ -152,7 +152,7 @@ void HAL_LPTIM_MspInit(LPTIM_HandleTypeDef* lptimHandle)
     */
     GPIO_InitStruct.Pin = Tach_FlowRate_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF14_LPTIM2;
     HAL_GPIO_Init(Tach_FlowRate_GPIO_Port, &GPIO_InitStruct);
--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -219,12 +219,19 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
     PA10     ------> TIM1_CH3
     PA11     ------> TIM1_CH4
     */
-    GPIO_InitStruct.Pin = PWM_Pump1_Pin|PWM_Pump2_Pin|PWM_BrakeLight_Pin|PWM_RadFan_Pin;
+    GPIO_InitStruct.Pin = PWM_Pump1_Pin|PWM_Pump2_Pin|PWM_BrakeLight_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = PWM_RadFan_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
+    HAL_GPIO_Init(PWM_RadFan_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN TIM1_MspPostInit 1 */
 
